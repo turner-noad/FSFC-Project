53358cc9668e02f8fe3a894b7f2177f922c5a164
Turner <turner.noad@gmail.com>
2022-10-01 22:59:41 +0100

    Basic Player Movement

--- Bin/player.js
+++ Bin/player.js
@@ -1,31 +1,44 @@
 class Player {
 	constructor() {
 		this.x_position = 225;
-		this.y_position = (height / 5) * 4.5;
+		this.y_position = Math.round(((height / 5) * 4.5) / 10) * 10;
 		this.horizontal_target = 225;
+		this.vertical_target = Math.round(((height / 5) * 4.5) / 10) * 10;
+		this.horizontal = 2;
+		this.vertical = 4.5;
 	}
 
 	show() {
-		fill(255);
-		noStroke();
 		if (this.horizontal_target > this.x_position) this.x_position += 10;
 		else if (this.horizontal_target < this.x_position) this.x_position -= 10;
+		if (this.vertical_target > this.y_position) this.y_position += 10;
+		else if (this.vertical_target < this.y_position) this.y_position -= 10;
+		fill(255);
+		noStroke();
 		circle(this.x_position, this.y_position, 50);
 	}
 
 	move(direction) {
 		if (direction == "left") {
-			if (this.x_position == 75) return this.shake();
+			if (this.x_position == 75) return this.shake("horizontal");
 			this.horizontal_target -= 150;
+			this.horizontal--;
 		} else if (direction == "right") {
-			if (this.x_position == 375) return this.shake();
+			if (this.x_position == 375) return this.shake("horizontal");
 			this.horizontal_target += 150;
+			this.horizontal++;
 		} else if (direction == "up") {
+			if (this.y_position == Math.round(((height / 5) * 0.5) / 10) * 10) return this.shake("vertical");
+			this.vertical -= 1;
+			this.vertical_target = Math.round(((height / 5) * this.vertical) / 10) * 10;
 		} else if (direction == "down") {
+			if (this.y_position == Math.round(((height / 5) * 4.5) / 10) * 10) return this.shake("vetical");
+			this.vertical += 1;
+			this.vertical_target = Math.round(((height / 5) * this.vertical) / 10) * 10;
 		}
 	}
 
-	shake() {
+	shake(axis) {
 		console.log("No");
 	}
 }
